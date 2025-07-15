bacd16194ec43880f44f556a290f4764af4a5ad1
fmoraless <franticzero@gmail.com>
2025-07-14 21:51:49 -0400

    Actualizar la documentación y mejorar la ruta de seed para crear múltiples entradas de Todo

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ docker compose up -d
 
 2. Renombrar el .env.template a .env
 3. Reemplazar las variables de entorno
+4. Ejecutar el seed  para [crear la base de datos local](localhost:3000/api/seed)
 
 
 # Prisma Commands
--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -2,14 +2,30 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const todo = await prisma.todo.create({
-    data: {
-      description: "Seeded Todo",
-      complete: false,
-    },
-  });
+  await prisma.todo.deleteMany();
 
-  console.log("Seeded Todo:", todo);
+  await prisma.todo.createMany({
+    data: [
+      {
+        description: "Piedra del alma",
+        complete: true,
+      },
+      {
+        description: "Piedra del poder",
+      },
+      {
+        description: "Piedra del tiempo",
+        complete: true,
+      },
+      {
+        description: "Piedra de la realidad",
+      },
+      {
+        description: "Piedra del espacio",
+        complete: true,
+      },
+    ],
+  });
 
   return NextResponse.json({
     message: "Seed endpoint",
