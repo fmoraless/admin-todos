429c6b2662874343bd8d29a1141d7519f89aa0a9
Francisco Morales Sánchez <franticzero@gmail.com>
2025-08-16 00:32:34 -0400

    Refactor Todo route to use a dedicated getTodo function and improve error handling

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,15 +1,25 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import * as yup from "yup";
+import { Todo } from "@/generated/prisma";
 
 interface Segments {
   params: {
     id: string;
   };
 }
+
+const getTodo = async (id: string): Promise<Todo | null> => {
+  const todo = await prisma.todo.findFirst({ where: { id } });
+
+  return todo;
+};
+
 export async function GET(request: Request, { params }: Segments) {
   const { id } = params;
 
-  const todo = await prisma.todo.findFirst({ where: { id } });
+  const todo = await getTodo(id);
+
   if (!todo) {
     return NextResponse.json(
       { message: `Todo con id: ${id} no encontrado` },
@@ -18,4 +28,37 @@ export async function GET(request: Request, { params }: Segments) {
   }
 
   return NextResponse.json(todo);
+}
+
+const putSchema = yup.object({
+  description: yup.string().optional(),
+  complete: yup.boolean().optional(),
+});
+
+export async function PUT(request: Request, { params }: Segments) {
+  const { id } = params;
+
+  const todo = await getTodo(id);
+
+  if (!todo) {
+    return NextResponse.json(
+      { message: `Todo con id: ${id} no encontrado` },
+      { status: 404 }
+    );
+  }
+
+  try {
+    const { complete, description } = await putSchema.validate(
+      await request.json()
+    );
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: { complete, description },
+    });
+
+    return NextResponse.json(updatedTodo);
+  } catch (error) {
+    return NextResponse.json(error.message, { status: 500 });
+  }
 }
