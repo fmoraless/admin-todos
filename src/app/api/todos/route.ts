07ed59d51ffc52d7875706ec05270848a5e18846
Francisco Morales Sánchez <franticzero@gmail.com>
2025-08-15 23:57:15 -0400

    Add Yup validation schema for POST request and update package dependencies

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import * as yup from "yup";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -28,12 +29,21 @@ export async function GET(request: Request) {
   return NextResponse.json(todos);
 }
 
+const postSchema = yup.object({
+  description: yup.string().required("La descripción es requerida"),
+  complete: yup.boolean().optional().default(false), //! TODO: mostrar algo interesante
+});
+
 export async function POST(request: Request) {
-  const body = await request.json();
+  try {
+    const body = await postSchema.validate(await request.json());
 
-  const todo = await prisma.todo.create({
-    data: body,
-  });
+    const todo = await prisma.todo.create({
+      data: body,
+    });
 
-  return NextResponse.json(todo, { status: 201 });
+    return NextResponse.json(todo, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
 }
