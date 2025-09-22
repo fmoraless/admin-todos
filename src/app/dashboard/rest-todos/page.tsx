940648938814ad3c12204df1e1dc0a1b9220afbb
Francisco Morales Sánchez <franticzero@gmail.com>
2025-09-21 21:09:19 -0300

    Add dashboard layout, sidebar, and widget components; update README and configuration

---
+++ src/app/dashboard/rest-todos/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function RestTodosPage() {
+  return (
+    <>
+      <span className="text-2xl font-bold">Rest todos</span>
+    </>
+  );
+}
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from "./Sidebar";
+export { SideBarItem } from "./SideBarItem";
+export { TopMenu } from "./TopMenu";
+export { WidgetItem } from "./WidgetItem";
