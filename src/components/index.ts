d1c2fa2c1bd4a4e1299dc68a8a570d156dac4662
Francisco Morales Sánchez <franticzero@gmail.com>
2025-09-22 01:10:46 -0300

    Refactor TodoItem component to use CSS modules for styling; add Tailwind CSS configuration file.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export * from "./Sidebar";
-export { SideBarItem } from "./SideBarItem";
+export { SidebarItem } from "./SidebarItem";
 export { TopMenu } from "./TopMenu";
 export { WidgetItem } from "./WidgetItem";
--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,10 +1,32 @@
 import { Todo } from "@/generated/prisma";
 import React from "react";
+import styles from "./TodoItem.module.css";
+import { IoCheckboxOutline } from "react-icons/io5";
 
 interface Props {
   todo: Todo;
 }
 
 export const TodoItem = ({ todo }: Props) => {
-  return <div>{todo.description}</div>;
+  return (
+    <div className={todo.complete ? styles.todoDone : styles.todoPending}>
+      <div className="flex flex-col sm:flex-row justify-items-start items-center gap-4">
+        <div
+          className={`
+        flex p-2 rounded-b-md cursor-pointer
+        hover:bg-opacity-60
+        ${todo.complete ? " bg-blue-100" : " bg-gray-100"}
+            `}
+        >
+          {todo.complete ? (
+            <IoCheckboxOutline size={20} className="text-green-600" />
+          ) : (
+            <IoCheckboxOutline size={20} className="text-gray-400" />
+          )}
+
+          <div className="text-center sm:text-left">{todo.description}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
