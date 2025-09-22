f83023a69e6d60b6409e6ef4903075c644df231d
Francisco Morales Sánchez <franticzero@gmail.com>
2025-09-22 00:12:06 -0300

    list rendering of todos

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,9 +1,17 @@
+import prisma from "@/lib/prisma";
 import React from "react";
 
-export default function RestTodosPage() {
+export const metadata = {
+  title: "Listado Rest Todos",
+  description: "Listado de tareas con Rest API",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
   return (
     <>
       <span className="text-2xl font-bold">Rest todos</span>
+      {JSON.stringify(todos, null, 2)}
     </>
   );
 }
