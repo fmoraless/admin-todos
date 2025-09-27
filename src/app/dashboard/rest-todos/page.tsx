8fd79fc0f2ed97ba648fa77ba46babea5313dd98
Francisco Morales Sánchez <franticzero@gmail.com>
2025-09-27 04:43:44 -0300

    Add NewTodo component and integrate it into RestTodosPage

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import { TodosGrid } from "@/todos";
+import { NewTodo } from "@/todos/components/NewTodo";
 import React from "react";
 
 export const metadata = {
@@ -11,6 +12,10 @@ export default async function RestTodosPage() {
   const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
   return (
     <div>
+      <div className="w-full px-3 mx-5 mb-5">
+        <NewTodo />
+      </div>
+
       <TodosGrid todos={todos} />
     </div>
   );
--- src/todos/helpers/todos.ts
+++ src/todos/helpers/todos.ts
@@ -19,3 +19,20 @@ export const updateTodo = async (
 
   return todo;
 };
+
+export const createTodo = async (description: string): Promise<Todo> => {
+  // Implementation for updating a todo
+  const body = { description };
+
+  const todo = await fetch("/api/todos", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  console.log({ todo });
+
+  return todo;
+};
