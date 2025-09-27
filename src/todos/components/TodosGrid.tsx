2f0ed006ec35806af158dd8cf3b03e6e976efe72
Francisco Morales Sánchez <franticzero@gmail.com>
2025-09-27 04:13:34 -0300

    refresh TodosGrid to use local toggleTodo function for updating todos

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -3,17 +3,26 @@ import { Todo } from "@/generated/prisma";
 import React from "react";
 import { TodoItem } from "./TodoItem";
 
-import * as api from "@/todos/helpers/todos";
+import * as todosApi from "@/todos/helpers/todos";
+import { useRouter } from "next/navigation";
 
 interface Props {
   todos?: Todo[];
 }
 
 export const TodosGrid = ({ todos = [] }: Props) => {
+  const router = useRouter();
+
+  const toggleTodo = async (id: string, complete: boolean) => {
+    await todosApi.updateTodo(id, complete);
+
+    router.refresh();
+  };
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} toggleTodo={api.updateTodo} />
+        <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
       ))}
     </div>
   );
