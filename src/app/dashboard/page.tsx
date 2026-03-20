8d35c6b5deec7c9dd943d9a6f569d58970eaf292
fmoraless-cydtec <fmorales@cydtec.com>
2026-03-20 20:44:11 -0300

    feat: integrate next-auth for authentication and enhance Dashboard and Sidebar components with user session management

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,24 @@
 import { WidgetItem } from "@/components";
+import { getServerSession } from "next-auth";
 import React from "react";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  const userName = session?.user?.name || "Desconocido";
+  const avatarUrl = session?.user?.image || "/default-avatar.png";
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <WidgetItem />
-      <WidgetItem />
-      <WidgetItem />
+      <WidgetItem title="usuario conectado S-Side">
+        {JSON.stringify(session.user)}
+      </WidgetItem>
     </div>
   );
 }
